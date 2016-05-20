023d5b217d5f58c401b79f52212a7c63192e3156
Assaf Gannon <assaf.gannon@gmai.com>
2016-05-20 13:16:48 +0300

    added a basic click event

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 class SearchBox extends React.Component {
+
+  handleClick(name, event) {
+    console.log(this);
+    console.log(event.target);
+    console.log(name);
+  }
+
   render() {
     return (
       <div>
         <input />
-        <button>Search</button>
+        <button onClick={this.handleClick.bind(this, 'button1')}>Search</button>
       </div>
     );
   }
